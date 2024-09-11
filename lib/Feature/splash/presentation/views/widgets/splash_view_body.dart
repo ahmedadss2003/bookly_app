43eb49bba0010cea85a5888ce419ab5c81008879
ahmed <ashrafahmed13222@gmail.com>
2024-09-11 12:03:59 +0300

    first commit

---
+++ lib/Feature/splash/presentation/views/widgets/splash_view_body.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class SplashViewBody extends StatelessWidget {
+  const SplashViewBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return  Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        Image.asset("assets/images/Logo.png"),
+        const SizedBox(height: 5,),
+        const Text(
+        "Read Free Books",
+        textAlign: TextAlign.center,
+        style: TextStyle(fontSize: 20),
+        ),
+      ],
+    );
+  }
+}
