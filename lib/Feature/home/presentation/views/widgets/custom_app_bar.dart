15e18b78d328147339e57438dc7ec9466f87fccb
ahmed <ashrafahmed13222@gmail.com>
2024-09-18 14:09:15 +0300

    create styles file and add tittle to home view

--- lib/Feature/home/presentation/views/widgets/custom_app_bar.dart
+++ lib/Feature/home/presentation/views/widgets/custom_app_bar.dart
@@ -8,7 +8,7 @@ class CustomAppbar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return  Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 24 , vertical: 40),
+      padding: const EdgeInsets.only( left: 10, top: 40 , bottom:40 ,right: 10 ),
       child: Row (
         children: [
           Image.asset(AssetsData.logo,height:20 ,),
