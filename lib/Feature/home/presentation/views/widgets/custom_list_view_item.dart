2f51e010f1cc4fe16d6b09b530fc75931bbcec23
ahmed <ashrafahmed13222@gmail.com>
2024-09-18 13:44:12 +0300

    create featured books listt view

--- lib/Feature/home/presentation/views/widgets/custom_list_view_item.dart
+++ lib/Feature/home/presentation/views/widgets/custom_list_view_item.dart
@@ -1,24 +1,21 @@
 import 'package:flutter/material.dart';
 
-class CustomListViewItem extends StatelessWidget {
-  const CustomListViewItem({super.key});
+class FeaturedListViewItem extends StatelessWidget {
+  const FeaturedListViewItem({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      height: MediaQuery.of(context).size.height*0.3,
-      child: AspectRatio( //to make the ui responsive  
-        aspectRatio: 2.7/4,
-        child: Container(
-          decoration:const BoxDecoration(
-            borderRadius: BorderRadius.all(Radius.circular(10)),
-            image: DecorationImage(
-              fit: BoxFit.fill,
-              image: NetworkImage("https://img.freepik.com/free-vector/hand-drawn-flat-design-stack-books-illustration_23-2149341898.jpg",)
-              ),
-          ),
-        )
-      ),
+    return AspectRatio( //to make the ui responsive  
+      aspectRatio: 2.7/4,
+      child: Container(
+        decoration:const BoxDecoration(
+          borderRadius: BorderRadius.all(Radius.circular(10)),
+          image: DecorationImage(
+            fit: BoxFit.fill,
+            image: NetworkImage("https://img.freepik.com/free-vector/hand-drawn-flat-design-stack-books-illustration_23-2149341898.jpg",)
+            ),
+        ),
+      )
     );
   }
 }
