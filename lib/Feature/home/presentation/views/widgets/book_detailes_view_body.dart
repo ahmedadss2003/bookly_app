f7ed8c7dedf77a8ae823012ae6e6449ce9d952b1
ahmed <ashrafahmed13222@gmail.com>
2024-09-19 21:28:51 +0300

    create custom Appbar Book Detailes

--- lib/Feature/home/presentation/views/widgets/book_detailes_view_body.dart
+++ lib/Feature/home/presentation/views/widgets/book_detailes_view_body.dart
@@ -1,3 +1,4 @@
+import 'package:bookly_app/Feature/home/presentation/views/widgets/custom_book_detailes_appbar.dart';
 import 'package:flutter/material.dart';
 
 class BookDetailesViewBody extends StatelessWidget {
@@ -6,7 +7,15 @@ class BookDetailesViewBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
-      
+      body: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 30,vertical: 20),
+        child: Column(
+          children: [
+            CustomBookDetailesAppbar(),
+          ],
+        ),
+      ),
     );
   }
 }
+
---
+++ lib/Feature/home/presentation/views/widgets/custom_book_detailes_appbar.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class CustomBookDetailesAppbar extends StatelessWidget {
+  const CustomBookDetailesAppbar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return  Row(
+      children: [
+        IconButton(onPressed: (){}, icon:const Icon(Icons.close),),
+        const Spacer(),
+        IconButton(onPressed: (){}, icon:const Icon(Icons.shopping_cart_outlined),),
+
+      ],
+    );
+  }
+}
