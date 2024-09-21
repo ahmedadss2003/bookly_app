2d40b75ca8865b484f9aa98cdb60354d7fc9d487
ahmed <ashrafahmed13222@gmail.com>
2024-09-21 18:45:43 +0300

    Create Book Model

--- lib/Feature/home/presentation/views/widgets/book_detailes_view_body.dart
+++ lib/Feature/home/presentation/views/widgets/book_detailes_view_body.dart
@@ -1,11 +1,7 @@
-import 'package:bookly_app/Feature/home/presentation/views/widgets/book_rating.dart';
-import 'package:bookly_app/Feature/home/presentation/views/widgets/custom_bar_books_detailes.dart';
 import 'package:bookly_app/Feature/home/presentation/views/widgets/custom_book_detailes_appbar.dart';
 import 'package:bookly_app/Feature/home/presentation/views/widgets/custom_book_detailes_component.dart';
-import 'package:bookly_app/Feature/home/presentation/views/widgets/custom_list_view_item.dart';
 import 'package:bookly_app/Feature/home/presentation/views/widgets/similer_books_component.dart';
-import 'package:bookly_app/Feature/home/presentation/views/widgets/similer_books_list_view.dart';
-import 'package:bookly_app/core/utilts/styles.dart';
+
 import 'package:flutter/material.dart';
 
 class BookDetailesViewBody extends StatelessWidget {
