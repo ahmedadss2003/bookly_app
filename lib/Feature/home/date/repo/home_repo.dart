60aace033359a49e89ac8508d8490bcd7b71e20e
ahmed <ashrafahmed13222@gmail.com>
2024-09-23 15:03:38 +0300

    Create Home repo And Failure File

--- lib/Feature/home/date/repo/home_repo.dart
+++ lib/Feature/home/date/repo/home_repo.dart
@@ -1,4 +1,8 @@
+import 'package:bookly_app/Feature/home/date/models/book_model/book_model.dart';
+import 'package:bookly_app/core/errors/failures.dart';
+import 'package:dartz/dartz.dart';
+
 abstract class HomeRepo { //this Abstract Class Use To Add The metod Without Implementation
-  fetchFeatureBooks() ;
-  fetchBestSallerBooks() ;
+  Future<Either<Failures , List<BookModel>>> fetchFeatureBooks();//please unerstand this Well
+  Future<Either<Failures , List<BookModel>>> fetchBestSallerBooks();
 }
---
+++ lib/core/errors/failures.dart
@@ -0,0 +1,7 @@
+abstract class Failures {
+
+}
+
+class NetworkFailure extends Failures{
+
+}
