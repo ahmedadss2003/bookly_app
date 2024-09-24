aa029575213acf1b1b1cf0a53920c312719cb274
ahmed <ashrafahmed13222@gmail.com>
2024-09-24 14:14:28 +0300

    create Newst Books Cubit

---
+++ lib/Feature/home/presentation/maneger/newst_books_cubit/newst_books_cubit.dart
@@ -0,0 +1,23 @@
+import 'package:bloc/bloc.dart';
+import 'package:bookly_app/Feature/home/date/models/book_model/book_model.dart';
+import 'package:bookly_app/Feature/home/date/repo/home_repo.dart';
+import 'package:equatable/equatable.dart';
+
+part 'newst_books_state.dart';
+
+class NewstBooksCubit extends Cubit<NewstBooksState> {
+  NewstBooksCubit(this.homeRepo) : super(NewstBooksInitial());
+  final HomeRepo homeRepo ;
+  Future<void> fetchNewstBooks()async{
+    emit(NewstBooksInitial());
+    var result =await homeRepo.fetchNewsedBooks();
+    result.fold(
+      (failure){
+        NewstBooksFailure(errMessage: failure.errMessage);
+      },
+      (books){
+        emit(NewstBooksSuccess(books: books)) ;
+      }
+      );
+  }
+}
